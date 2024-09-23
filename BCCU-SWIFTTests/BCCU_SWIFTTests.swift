a570e0beacbd0fdd40723d1d883cf8c1d8801ab6
Jack Tolley <jacktolley.1007@gmail.com>
2024-09-23 16:37:33 +0100

    Initial Commit

---
+++ BCCU-SWIFTTests/BCCU_SWIFTTests.swift
@@ -0,0 +1,17 @@
+//
+//  BCCU_SWIFTTests.swift
+//  BCCU-SWIFTTests
+//
+//  Created by Jack Tolley on 23/09/2024.
+//
+
+import Testing
+@testable import BCCU_SWIFT
+
+struct BCCU_SWIFTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BCCU-SWIFTUITests/BCCU_SWIFTUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BCCU_SWIFTUITestsLaunchTests.swift
+//  BCCU-SWIFTUITests
+//
+//  Created by Jack Tolley on 23/09/2024.
+//
+
+import XCTest
+
+final class BCCU_SWIFTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
